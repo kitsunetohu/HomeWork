6e8f0f89b7ae30120c49c198b145a4d63909d8fe
wkmkymt <wkmkymt@gmail.com>
2019-05-13 20:26:15 +0900

    [change] Improvement Controll Method of Camera Angle

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,31 +4,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Max Vertical Angle
-    public float maxAngleV;
-    // Min Vertical Angle
-    public float minAngleV;
-    // Max Horizontal Angle
-    public float maxAngleH;
-    // Min Horizontal Angle
-    public float minAngleH;
-    // Rotational Speed
-    public float speed;
+    
+    public float maxAngleV;           // Max Vertical Angle
+    public float minAngleV;           // Min Vertical Angle
+    public float maxAngleH;           // Max Horizontal Angle
+    public float minAngleH;           // Min Horizontal Angle
+    public float speed;               // Rotational Speed
 
-    // Now Vertical Angle
-    private float nowAngleV;
-    // Player Position
-    private Vector3 PlayerPosition;
+    private Vector3 PlayerPosition;   // Player Position
     
 
     void Start() {
         // Initialize
-        maxAngleV      = 80;
-        minAngleV      = -80;
+        maxAngleV      = 280;
+        minAngleV      = 80;
         maxAngleH      = 280;
         minAngleH      = 80;
         speed          = 50;
-        nowAngleV      = 0;
         PlayerPosition = transform.position;
     }
 
@@ -36,21 +28,17 @@ public class PlayerController : MonoBehaviour
         float addRot = speed * Time.deltaTime;
 
         Debug.Log("-----------------------------------------");
-        Debug.Log(isPlayerFront());
+        Debug.Log(transform.localEulerAngles.x);
         
         // Move Up
-        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {
-            nowAngleV = Mathf.Clamp(nowAngleV - addRot, minAngleV, maxAngleV);
-            if(nowAngleV > minAngleV)
+        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+            if(minAngleV > transform.localEulerAngles.x - addRot || transform.localEulerAngles.x - addRot > maxAngleV)
                 transform.RotateAround(PlayerPosition, transform.right, -addRot);
-        }
 
         // Move Down
-        if(Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) {
-            nowAngleV = Mathf.Clamp(nowAngleV + addRot, minAngleV, maxAngleV);
-            if(nowAngleV < maxAngleV)
+        if(Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+            if(minAngleV > transform.localEulerAngles.x + addRot || transform.localEulerAngles.x + addRot > maxAngleV)
                 transform.RotateAround(PlayerPosition, transform.right, addRot);
-        }
 
         // Move Right
         if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
