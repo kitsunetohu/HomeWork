f75981d4841e2f23e790183ed6bd5e2c9f37d7ec
CONG XI <45125560+kitsunetohu@users.noreply.github.com>
2019-05-13 18:25:22 +0900

    0

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -8,7 +8,7 @@ public class UIManager : Manager<UIManager>
 {
     public Graphic talkPanel;
     public CanvasGroup loadingPanel;
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,23 +18,30 @@ public class UIManager : Manager<UIManager>
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    public void TalkUIUpdata(string talk){
-        if(talkPanel.gameObject.activeSelf!=true){
+    public void TalkUIUpdata(string talk)
+    {
+        if (talkPanel.gameObject.activeSelf != true)
+        {
             //TalkPanelを開く
             talkPanel.gameObject.SetActive(true);
         }
         //トークUIを更新する
-        talkPanel.GetComponentInChildren<Text>().text=talk;
+        talkPanel.GetComponentInChildren<Text>().text = talk;
     }
-    public void hideTalk(){
+    public void hideTalk()
+    {
         //トークUIを隠す
         talkPanel.gameObject.SetActive(false);
     }
 
-    public void LoadingFade(){
-        loadingPanel.DOFade(0,2);
+    public void LoadingFade()
+    {
+        loadingPanel.DOFade(0, 2).OnComplete(() =>
+        {
+            loadingPanel.gameObject.SetActive(false);
+        });
     }
 }
