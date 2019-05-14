961f9a42bca2cf2ec8cef28f177e2eacec39a3f1
wkmkymt <wkmkymt@gmail.com>
2019-05-14 19:32:55 +0900

    [add] Add Enemy Class

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private float   health;      // Enemy's HP
+    private float damageRate;
+
+    void Start() {
+        // Temp
+        health     = 100;
+        damageRate = 50;
+    }
+
+    void Update() {
+    }
+
+    public void GetDamage() {
+        health -= damageRate * Time.deltaTime;
+        if(health < 0)
+            this.Death();
+    }
+
+    private void Death() {
+        gameObject.SendMessage("Killedenemy");
+        Destroy(gameObject);
+    }
+}
